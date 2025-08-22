49ff61f0bde6e7abee26fe70a319dbb975caafe8
Edmure Pizza <edmurepi@un-c5.kzn.21-school.ru>
2025-08-22 21:09:40 +0300

    materials++

--- internal/my_mutex_channel/mutex.go
+++ internal/my_mutex_channel/mutex.go
@@ -1,1 +1,18 @@
 package mymutexchannel
+
+// Реализовать мьютекс с FIFO очередью на каналах
+type Mutex struct {
+	chanQueue chan struct{}
+}
+
+func NewMutex() *Mutex {
+	// TODO: инициализация
+}
+
+func (m *Mutex) Lock() {
+	// TODO: встать в очередь
+}
+
+func (m *Mutex) Unlock() {
+	// TODO: разбудить следующего
+}
--- internal/my_mutex_spinlock/mutex.go
+++ internal/my_mutex_spinlock/mutex.go
@@ -1,1 +1,16 @@
 package mymutexspinlock
+
+import "sync/atomic"
+
+// Реализовать спинлок на основе атомарных операций
+type Mutex struct {
+	locked atomic.Bool
+}
+
+func (s *Mutex) Lock() {
+	// TODO: реализовать захват через цикл ожидания
+}
+
+func (s *Mutex) Unlock() {
+	// TODO: реализовать освобождение
+}
