dd5d41a298c533bef267f5c096da4b57907bfbe7
Edmure Pizza <edmurepi@un-c5.kzn.21-school.ru>
2025-08-22 18:44:14 +0300

    my_mutex started

---
+++ cmd/test_field/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I will try to implement popular variants of mutex implementation in go")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module my_mutex
+
+go 1.22.3
