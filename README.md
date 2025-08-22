d0468e1188659537ea58c112bdffe69efe470bdc
Edmure Pizza <edmurepi@un-c5.kzn.21-school.ru>
2025-08-22 18:49:23 +0300

    my_mutex started

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # my_mutex
 I will try to implement popular variants of mutex implementation in go
+
+Вариант 1: Channel
+Вариант 2: Petersons
+Вариант 3: Spinlock
+Вариант 4: Ticket
--- cmd/test_field/main.go
+++ cmd/test_field/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+func testMutex(mutex sync.Locker, name string) {
+	var counter int
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mutex.Lock()
+			counter++
+			mutex.Unlock()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("%s: counter = %d (expected: 1000)\n", name, counter)
+}
 
 func main() {
-	fmt.Println("I will try to implement popular variants of mutex implementation in go")
+	// Протестировать все реализации
+	testMutex(&Spinlock{}, "Spinlock")
+	// testMutex(NewChannelMutex(), "ChannelMutex")
+	// ... другие тесты
 }
+
+/*
+	# BONUS LEVEL
+*/
+/*
+ - Добавить TryLock() - неблокирующая попытка захвата
+ - Реентерабельный мьютекс - с поддержкой повторного захвата
+ - RW-мьютекс - с разделением на читателей и писателей
+ - Таймауты - LockWithTimeout(time.Duration) bool
+*/
